ebd0a70f720216d7bb0c77239cad0815143f1aa1
Megha Mapalagama <meghamapalagama@meghas-mbp.home>
2021-02-26 11:32:20 -0500

    second commit

--- src/welcome5/HelloWorld.java
+++ src/welcome5/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println(" Welcome to Hello World 5  ! ");
+		
+		System.out.println("World climate chnge we shold think about it");
 
 	}
 
